9eaf4685c9cf3411ed6a35482a5f9950eba7d1f0
Matthew Schupack <schu34@gmail.com>
2018-12-21 16:34:11 -0500

    working functions

--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,25 @@
 exports.handler = (event, ctx, cb) => {
-  console.log(event, ctx);
-   cb(null,{ statusCode: 200 });
+	console.log(event, ctx);
+	switch (event.httpMethod) {
+		case "GET":
+			getHandler(event, ctx, cb);
+			break;
+		case "POST":
+			postHandler(event, ctx, cb);
+			break;
+		default: 
+			break;
+	}
+}
+
+function postHandler(){};
+function getHandler(e, c, cb) {
+
+	cb(null, {
+		statusCode: 301,
+		body: '',
+		headers: {
+			Location: 'http://www.google.com'
+		}
+	});
 };
